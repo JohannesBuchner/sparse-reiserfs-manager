c262abdccb45fd077618666c4efa5abab680ba3f
Johannes Buchner <johannes.buchner.acad@gmx.com>
2013-02-06 00:05:43 +0100

    added license and short description

--- subfs.sh
+++ subfs.sh
@@ -1,7 +1,35 @@
 #!/bin/bash
-#
-# Johannes Buchner (C) 2013
 # 
+# Manager to create and resize reiserfs sub-filesystems, which are stored in 
+# sparse files.
+# 
+# -----------------------------------------------------------------------------
+#
+# Copyright (c) 2013, Johannes Buchner
+# All rights reserved.
+#
+# Redistribution and use in source and binary forms, with or without 
+# modification, are permitted provided that the following conditions are met:
+#
+#    Redistributions of source code must retain the above copyright notice, 
+#      this list of conditions and the following disclaimer.
+#    Redistributions in binary form must reproduce the above copyright notice, 
+#      this list of conditions and the following disclaimer in the 
+#      documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" 
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE 
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE 
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE 
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR 
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF 
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS 
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN 
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) 
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE 
+# POSSIBILITY OF SUCH DAMAGE.
+# 
+# -----------------------------------------------------------------------------
 
 function usage() {
 	echo "SYNAPSIS: $0 <mountpoint> <command> <size>"
@@ -10,7 +38,7 @@ function usage() {
 	echo ""
 	echo ""
 	echo "This script creates and manages a reiser filesystem and mounts it at mountpoint"
-	echo "The data file used will be stored at <mountpoint>.fs"
+	echo "The sparse data file used will be stored at <mountpoint>.fs"
 	echo "You will need to run this script as root (for mounting)"
 	echo ""
 	echo "commands:"
